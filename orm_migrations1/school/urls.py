4900815dd290edf944210366ca56cef255629688
balabasta12 <balabasta12@gmail.com>
2022-04-14 20:03:34 +0300

    Готово

---
+++ orm_migrations1/school/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from school.views import students_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', students_list, name='students'),
+
+
+]
